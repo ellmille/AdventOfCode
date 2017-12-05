bdfe78e785e3576e98f65d5d6f07f93cf599ede8
ellmille <ellelynn98@gmail.com>
2017-12-05 09:00:58 -0600

    solved day 2

--- day2.js
+++ day2.js
@@ -1,6 +1,4 @@
-//Corruption Checksum
-//http://adventofcode.com/2017/day/2
-//node day2.js input.txt
+//Corruption Checksum -- http://adventofcode.com/2017/day/2
 
 //The spreadsheet consists of rows of apparently-random numbers. 
 //To make sure the recovery process is on the right track, 
@@ -17,11 +15,95 @@
 //ex. The third row's difference is 6.
 //ex. The spreadsheet's checksum would be 8 + 4 + 6 = 18.
 const fs = require('fs');
+const readLine = require('readline');
+const stream = require('stream');
 
-const inputFile = process.argv[2];
-fs.createReadStream(inputFile).pipe(process.stdout);
+//const inputFile = process.argv[2];
+const input = fs.createReadStream("day2Input.txt");
+const lineReader = readLine.createInterface(input);
 
-//parse input into rows
-//iterate through rows find max and min values
-//find difference of max and min in each row
-//find sum of all differences
+var inputArray = [];
+
+lineReader.on('line', function (line) {
+    inputArray.push(line);
+});
+
+lineReader.on('close', function () {
+    console.log(stepOne(inputArray));
+    console.log(stepTwo(inputArray));
+})
+
+function stepOne(input) {
+    let sum = 0;
+    //iterate through the rows
+    for (let i = 0; i < input.length; i++) {
+        //split by / -- tabs are empty
+        let rowArray = input[i].split("\t");
+        //find min and max
+        let min = findMinInArray(rowArray);
+        let max = findMaxInArray(rowArray);
+        //find difference and add to sum
+        sum += (max - min);
+    }
+    return sum;
+}
+
+function findMaxInArray(array) {
+    return Math.max.apply(null, array);
+}
+
+function findMinInArray(array) {
+    return Math.min.apply(null, array);
+}
+
+//The goal is to find the only two numbers in each row where one evenly divides the other
+//that is, where the result of the division operation is a whole number. 
+//They would like you to find those numbers on each line, divide them, and add up each line's result.
+
+function stepTwo(input) {
+    let sum = 0;
+    for (let i = 0; i < input.length; i++) {
+        let rowArray = input[i].split("\t");
+        //find divisible numbers
+        if (rowArray) {
+            sum += findRowDifference(cleanArray(rowArray));
+        }
+    }
+    return sum;
+}
+
+function findRowDifference(rowArray) {
+    //compare [0] to [1] then [0] to [2] ect ect
+    for (let i = 0; i < rowArray.length; i++) {
+        let compare = compareToArray(rowArray[i], i, rowArray);
+        if (compare) {
+            return compare;
+        }
+    }
+}
+
+function compareToArray(n, arrayPlace, rowArray) {
+    for (let i = 0; i < rowArray.length; i++) {
+        if (i !== arrayPlace) {
+            if (n % rowArray[i] == 0) {
+                return n / rowArray[i];
+            } else if (rowArray[i] % n == 0) {
+                return rowArray[i] / n;
+            }
+        }
+    }
+}
+
+function cleanArray(rowArray) {
+    let cleanArray = [];
+    for (let i = 0; i < rowArray.length; i++) {
+        if (isNumber(rowArray[i])) {
+            cleanArray.push(parseInt(rowArray[i]));
+        }
+    }
+    return cleanArray;
+}
+
+function isNumber(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
