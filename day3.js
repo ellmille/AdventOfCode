6bad548661db6837acbec83d5568926a77a6d7f4
ellmille <ellelynn98@gmail.com>
2017-12-05 21:35:04 -0600

    added day 4

--- day3.js
+++ day3.js
@@ -18,12 +18,16 @@
 //How many steps are required to carry the data from the puzzle input all the way to the access port
 const input = 361527;
 
-console.log(stepOne(input));
+//console.log(stepOne(input));
+stepTwo(input);
 
 function stepOne(input){
     var result = createSpiral(input);
     var singleSpiral = result[input - 1].getLocation();
     return findDistance(singleSpiral[0], singleSpiral[1]);
+}
+function stepTwo(input){
+    var spiral = createSumSpiral(input);
 }
 
 function createSpiral(length){ 
@@ -75,4 +79,124 @@ function square(x, y, i){
     this.getLocation = function(){
         return [this.x, this.y];
     }
+    this.getValue = function(){
+        return this.value;
+    }
+    this.findByLocation = function(x, y){
+        if(x == this.x && y == this.y){
+            return this;
+        }
+        return false;
+    }
+}
+
+//The programs here clear the grid and then store the value 1 in square 1. 
+//Then, in the same allocation order as shown above, 
+//they store the sum of the values in all adjacent squares, including diagonals.
+//147  142  133  122   59
+//304    5    4    2   57
+//330   10    1    1   54
+//351   11   23   25   26
+//362  747  806--->   ...
+//What is the first value written that is larger than your puzzle input?
+function createSumSpiral(length){ 
+    var spiral = [];
+    spiral.push(new square(0, 0, 1));
+    var x = 1;
+    var y = 0;
+    var i = 2;
+
+    let sideLength = 2;
+    while(i < length){
+        //create right (bottom to top);
+        for(let j = 0; j < sideLength - 1; j++){
+            let value = findSquareValue(spiral, x, y);
+            if(checkForResult(value)){
+                return;
+            }
+            spiral.push(new square(x, y, value));
+            i++;
+            y++;
+        }
+        //create top (right to left)
+        for(let j = 0; j < sideLength; j++){
+            let value = findSquareValue(spiral, x, y);            
+            if(checkForResult(value)){
+                return;
+            }
+            spiral.push(new square(x, y, value));
+            i++;
+            x--;
+        }
+        //create left (top to bottom)
+        for(let j = 0; j < sideLength; j++){
+            let value = findSquareValue(spiral, x, y);            
+            if(checkForResult(value)){
+                return;
+            }
+            spiral.push(new square(x, y, value));
+            i++;
+            y--;
+        }
+        //create bottom (left to right) and add an extra to the right
+        for(let j = 0; j < sideLength + 1; j++){
+            let value = findSquareValue(spiral, x, y);            
+            if(checkForResult(value)){
+                return;
+            }
+            spiral.push(new square(x, y, value));
+            i++;
+            x++;
+        }
+        sideLength += 2;
+    }
+    return spiral;
+}
+
+function findSquareValue(squareArray, x, y){
+    let sum = 0;
+    //find all squares around current square
+    for(let i = 0; i < squareArray.length; i++){
+        //find square to the right
+        if(squareArray[i].findByLocation(x + 1, y)){
+            sum += squareArray[i].getValue();
+        }
+        //upper right
+        if(squareArray[i].findByLocation(x + 1, y + 1)){
+            sum += squareArray[i].getValue();
+        }
+        //above
+        if(squareArray[i].findByLocation(x, y + 1)){
+            sum += squareArray[i].getValue();
+        }
+        //upper left
+        if(squareArray[i].findByLocation(x - 1, y + 1)){
+            sum += squareArray[i].getValue();
+        }
+        //left
+        if(squareArray[i].findByLocation(x - 1, y)){
+            sum += squareArray[i].getValue();
+        }
+        //lower left
+        if(squareArray[i].findByLocation(x - 1, y - 1)){
+            sum += squareArray[i].getValue();
+        }
+        //lower
+        if(squareArray[i].findByLocation(x, y - 1)){
+            sum += squareArray[i].getValue();
+        }
+        //lower right
+        if(squareArray[i].findByLocation(x + 1, y - 1)){
+            sum += squareArray[i].getValue();
+        }
+    }
+    return sum;
+}
+
+function checkForResult(value){
+    if(value > input){
+        console.log(value);
+        return true;
+    }
+    return false;
 }
